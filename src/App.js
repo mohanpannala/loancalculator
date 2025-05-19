643afd5514de9121c32619f7c71614ef7d000705
mohanpannala <pannalamohan707@gmail.com>
2025-05-19 20:06:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { CssBaseline } from '@mui/material';
-// import { ColorModeProvider } from './contexts/ColorModeContext.js';
-// import ExchangeRates from './components/ExchangeRates';
-// import ErrorBoundary from './components/ErrorBoundary';
-// import Navbar from './components/Navbar';
-// import LoanCalculator from './components/LoanCalculator';
-// import About from './components/About';
-// import ErrorPage from './pages/ErrorPage';
-
-
-// const App = () => (
-  
-//   <ColorModeProvider>
-//     <CssBaseline />
-//     <ErrorBoundary>
-//       <Router>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<LoanCalculator />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="*" element={<ErrorPage />} />
-//           <Route path="/exchange-rates" element={<ExchangeRates />} />
-//           <Route path="/errorpage" element={<ErrorPage />} />
-//         </Routes>
-//       </Router>
-//     </ErrorBoundary>
-//   </ColorModeProvider>
-// );
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ColorModeProvider } from './contexts/ColorModeContext'; // updated context using plain CSS
